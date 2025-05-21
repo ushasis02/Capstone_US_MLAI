d7c4f773a485e7045c0897d07be26f890e8e00d5
ushasis02 <93474220+ushasis02@users.noreply.github.com>
2025-05-21 22:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ def recommend_top5():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use PORT from Heroku, fallback to 5000 for local
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))  # Use PORT from Heroku, fallback to 5000 for local
+    app.run(debug=True)
     
