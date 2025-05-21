9ecb34e7e908b61b8d820aae83d219fb353acd4e
ushasis02 <93474220+ushasis02@users.noreply.github.com>
2025-05-21 22:19:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #from cgitb import text
 from flask import Flask,render_template,request
+import os
 import model 
 app = Flask('__name__')
 
@@ -26,6 +27,6 @@ def recommend_top5():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.debug=False
-
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Heroku, fallback to 5000 for local
+    app.run(host='0.0.0.0', port=port)
+    
